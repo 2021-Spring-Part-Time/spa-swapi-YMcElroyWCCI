5044524ecc020c7f268607a571e027f1bc031cdc
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-07-10 10:33:32 -0400

    create Footer component and reference it in index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 buildPage();
 
 function buildPage(){
     header();
+    footer();
 }
+
 
 function header(){
     const headerElement = document.querySelector('.header');
     headerElement.innerHTML = Header();
+}
+
+function footer(){
+    const headerElement = document.querySelector('.footer');
+    headerElement.innerHTML = Footer();
 }
