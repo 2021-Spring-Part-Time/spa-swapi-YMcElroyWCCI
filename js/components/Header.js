bf0f0398e77c66a342645fa955690471dea6acca
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-07-10 10:20:48 -0400

    create Header components

---
+++ js/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return `
+    <ul class='nav__list'>
+        <li class='nav__list__home'>Home</li>
+        <li class='nav__list__people'>People</li>
+        <li class='nav__list__films'>Films</li>
+    </ul>
+    `;
+}
